d6e047cf7a1ead1c54db94de45d82642262753fc
Julian <reutherjulian@gmail.com>
2020-01-07 09:00:26 +0100

    DB-Name, User und Passwort gesetzt

--- RESTWebService/src/Customizing/Customizing.java
+++ RESTWebService/src/Customizing/Customizing.java
@@ -2,7 +2,7 @@ package Customizing;
 
 public class Customizing {
 
-	public static final String URL = "jdbc:postgresql://localhost:5432/testdb";
-	public static final String USER = "user";
-	public static final String PASSWORD = "password";
+	public static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+	public static final String USER = "postgres";
+	public static final String PASSWORD = "Trew1234";
 }
