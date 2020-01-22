2672c782e6860f02f10545575b535233c99bf645
baudi94 <58433364+baudi94@users.noreply.github.com>
2020-01-22 17:14:26 +0100

    Hamburger Menu added

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -7,5 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+
+  navbarOpen = false;
+
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
   
 }
